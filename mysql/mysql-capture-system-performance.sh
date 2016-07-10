7f7fe2e203b1a46a66f8d512b7749301ed038e07
noprom <tyee.noprom@qq.com>
2016-07-10 14:59:27 +0800

    check params

--- mysql/mysql-capture-system-performance.sh
+++ mysql/mysql-capture-system-performance.sh
@@ -1,19 +1,29 @@
 #!/bin/sh
 
+# 检查传递的参数
+if [ $# != 2 ] ; then 
+	echo "USAGE: $0 username password" 
+	echo " e.g.: $0 root root" 
+	exit 1; 
+fi
+
+# 用户名和密码
+USERNAME=$1
+PASSWORD=$2
 INTERVAL=5
 PREFIX=$INTERVAL-sec-status
 RUNFILE=/home/benchmarks/running
-mysql -uroot -p1q2w3e4r5t -e 'SHOW GLOBAL VARIABLES' >> mysql-variables
+mysql -u$USERNAME -p$PASSWORD -e 'SHOW GLOBAL VARIABLES' >> mysql-variables
 while test -e $RUNFILE; do
 	file=$(date +%F_%I)
 	sleep=$(date +%s.%N | awk "{print $INTERVAL - (\$1 % $INTERVAL)}") sleep $sleep
 	ts="$(date +"TS %s.%N %F %T")"
 	loadavg="$(uptime)"
 	echo "$ts $loadavg" >> $PREFIX-${file}-status
-	mysql -e 'SHOW GLOBAL STATUS' >> $PREFIX-${file}-status &
+	mysql -u$USERNAME -p$PASSWORD -e 'SHOW GLOBAL STATUS' >> $PREFIX-${file}-status &
 	echo "$ts $loadavg" >> $PREFIX-${file}-innodbstatus
-	mysql -e 'SHOW ENGINE INNODB STATUS\G' >> $PREFIX-${file}-innodbstatus & 
+	mysql -u$USERNAME -p$PASSWORD -e 'SHOW ENGINE INNODB STATUS\G' >> $PREFIX-${file}-innodbstatus & 
 	echo "$ts $loadavg" >> $PREFIX-${file}-processlist
-	mysql -e 'SHOW FULL PROCESSLIST\G' >> $PREFIX-${file}-processlist & echo $ts
+	mysql -u$USERNAME -p$PASSWORD -e 'SHOW FULL PROCESSLIST\G' >> $PREFIX-${file}-processlist & echo $ts
 done
 echo Exiting because $RUNFILE does not exist.
