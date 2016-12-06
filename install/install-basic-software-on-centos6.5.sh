708f6e0cdb6852786612bc961356f9b8c5711e53
noprom <tyee.noprom@qq.com>
2016-12-06 18:16:33 +0800

    add init script

--- install/install-basic-software-on-centos6.5.sh
+++ install/install-basic-software-on-centos6.5.sh
@@ -14,4 +14,4 @@ yum -y install nc htop iotop iftop telnet wget curl
 yum -y remove mysql-server mysql httpd
 yum -y install ImageMagick-*
 yum -y install nginx Percona-Server-server-55 xtrabackup
-yum -y install php55w php55w-fpm php55w-common php55w-cli php55w-devel php55w-intl php55w-mysqlnd php55w-pdo php55w-soap php55w-tidy php55w-xml php55w-xmlrpc php55w-zts php55w-gd php55w-mbstring php55w-mcrypt php55w-pecl-zendopcache php55w-pear php55w-posix php55w-pecl-memcached php55w-pecl-memcache php55w-pecl-memcached php55w-mysqlnd php55w-pecl-redis
+yum -y install php70w php70w-opcache php70w-fpm php70w-common php70w-cli php70w-devel php70w-intl php70w-mysqlnd php70w-pdo php70w-soap php70w-tidy php70w-xml php70w-xmlrpc php70w-zts php70w-gd php70w-mbstring php70w-mcrypt php70w-pecl-zendopcache php70w-pear php70w-posix php70w-pecl-memcached php70w-pecl-memcache php70w-pecl-memcached php70w-mysqlnd php70w-pecl-redis
